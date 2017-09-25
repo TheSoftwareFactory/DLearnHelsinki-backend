348fc90672fcf1a331f1b8098cec595a9abcc4df
Delma <delma@del.ma>
2017-09-25 15:10:50 +0300

    Security with Spring Security.

---
+++ src/main/java/org/dlearn/helsinki/skeleton/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package org.dlearn.helsinki.skeleton;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    
+    public SecurityWebApplicationInitializer() {
+        super(SpringSecurityConfig.class);
+    }
+}
