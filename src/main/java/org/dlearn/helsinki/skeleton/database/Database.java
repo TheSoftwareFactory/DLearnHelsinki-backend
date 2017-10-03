601474f9a76cb5e952da48a29e68967d63cd9fab
Delma <delma@del.ma>
2017-10-03 18:21:37 +0300

    Fixed?

--- src/main/java/org/dlearn/helsinki/skeleton/database/Database.java
+++ src/main/java/org/dlearn/helsinki/skeleton/database/Database.java
@@ -208,37 +208,23 @@ public class Database extends AbstractDataSource {
 /////////////////////////////////////////////////////////
     private static Connection getDBConnection() {
         Connection dbConnection = null;
-        System.out.println("1");
         try {
             Class.forName(DB_DRIVER);
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("2");
         try {
             String dbUrl = System.getenv("JDBC_DATABASE_URL");
-            String dbLogin = System.getenv("JDBC_DATABASE_LOGIN");
-            String dbPassword = System.getenv("JDBC_DATABASE_PASSWORD");
             if(dbUrl == null){ // local # TODO fix
-                System.out.println("4");
             	System.out.println("JDBC env empty, on local");
                 dbConnection = DriverManager.getConnection(
                         DB_CONNECTION, DB_USER, DB_PASSWORD);
-            }else if(dbLogin == null && dbPassword == null) { // production
-                System.out.println("5");
+            }else { // production
             	dbConnection = DriverManager.getConnection(dbUrl);
-            } else {
-                System.out.println("6");
-            	//"jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true"
-            	dbUrl += "?user=" + dbLogin + "&password=" + dbPassword + "&ssl=true";
-            	//dbConnection = DriverManager.getConnection(dbUrl, dbLogin, dbPassword);
-            	dbConnection = DriverManager.getConnection(dbUrl);
-            };
+            }
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            System.out.println("7");
+            System.out.println("CREATING CONNECTION FAILED HORRIBLY " + e.getMessage() + " (fix pls)");
         }
-        System.out.println("8");
         return dbConnection;
 
     }
