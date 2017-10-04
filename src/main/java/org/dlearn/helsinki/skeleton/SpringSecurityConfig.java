ac3dcd28d3390ce4189e7a0e41c408a16ca78915
Delma <delma@del.ma>
2017-10-04 11:01:39 +0300

    Fixing more SQL.

--- src/main/java/org/dlearn/helsinki/skeleton/SpringSecurityConfig.java
+++ src/main/java/org/dlearn/helsinki/skeleton/SpringSecurityConfig.java
@@ -20,14 +20,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication()
             .dataSource(db)
             .usersByUsernameQuery("select * from ("
-                        + "select username, pwd, 'true' as enabled from \"Students\""
+                        + "select username, pwd, 'true' as enabled from public.\"Students\""
                         + " union "
-                        + "select username, pwd, 'true' as enabled from \"Teachers\""
+                        + "select username, pwd, 'true' as enabled from public.\"Teachers\""
                     + ") A where username=?")
             .authoritiesByUsernameQuery("select * from ("
-                        + "select username, 'ROLE_STUDENT' from \"Students\""
+                        + "select username, 'ROLE_STUDENT' from public.\"Students\""
                         + "union"
-                        + "select username, 'ROLE_TEACHER' from \"Teachers\""
+                        + "select username, 'ROLE_TEACHER' from public.\"Teachers\""
                     + ") A where username=?")
             .passwordEncoder(new BCryptPasswordEncoder(16))
             .and()
