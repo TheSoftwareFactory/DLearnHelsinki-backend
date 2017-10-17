55b20d641ab47a505d6db09fb8bd76fbdbc86165
Delma <delma@del.ma>
2017-10-17 14:47:10 +0300

    Return groups student is in when changing group.

--- src/main/java/org/dlearn/helsinki/skeleton/resource/TeacherClassStudentResource.java
+++ src/main/java/org/dlearn/helsinki/skeleton/resource/TeacherClassStudentResource.java
@@ -8,10 +8,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.dlearn.helsinki.skeleton.model.Group;
 import org.dlearn.helsinki.skeleton.model.ListStudentThemeAverage;
 
 import org.dlearn.helsinki.skeleton.model.Student;
 import org.dlearn.helsinki.skeleton.model.StudentThemeAverage;
+import org.dlearn.helsinki.skeleton.service.GroupService;
 import org.dlearn.helsinki.skeleton.service.MoveToGroupService;
 import org.dlearn.helsinki.skeleton.service.ProgressionService;
 import org.dlearn.helsinki.skeleton.service.TeacherClassStudentService;
@@ -22,6 +24,7 @@ public class TeacherClassStudentResource {
     private final TeacherClassStudentService teacherClassStudent = new TeacherClassStudentService();
     private final MoveToGroupService moveToGroup = new MoveToGroupService();
     private final ProgressionService progression = new ProgressionService();
+    private final GroupService group = new GroupService();
 
     // GET student info /{student_id}/
 
@@ -37,13 +40,15 @@ public class TeacherClassStudentResource {
             @PathParam("student_id") int student_id) {
         return teacherClassStudent.getStudentThemeAverage(survey_id, student_id);
     }
-    
+
     @POST
     @Path("/{student_id}/move_to_group/{group_id}")
-    public void change_group(@PathParam("class_id") int class_id, @PathParam("student_id") int student_id, @PathParam("group_id") int group_id) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Group> change_group(@PathParam("class_id") int class_id, @PathParam("student_id") int student_id, @PathParam("group_id") int group_id) {
         moveToGroup.moveStudentToGroup(class_id, student_id, group_id);
+        return group.getAllGroupsTheStudentIsIn(student_id);
     }
-    
+
     @GET
     @Path("/{student_id}")
     public Student getStudent(
@@ -52,7 +57,7 @@ public class TeacherClassStudentResource {
         System.out.println("fetching student");
         return teacherStudentService.getStudent(student_id);
     }
-    
+
     @GET
     @Path("/{student_id}/progression/{amount}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -62,7 +67,7 @@ public class TeacherClassStudentResource {
             @PathParam("amount") int amount) {
         return progression.getStudentClassProgression(class_id, student_id, amount);
     }
-    
+
     @GET
     public List<Student> getListOfStudents(@PathParam("class_id") int class_id) {
         System.out.println("fetching list of students from class");
