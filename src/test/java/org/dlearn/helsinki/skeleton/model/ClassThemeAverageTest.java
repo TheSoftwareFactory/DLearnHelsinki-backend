48e07b0086098c584a3ac29582da008c6d15c9ac
mihassin <marko.hassinen@helsinki.fi>
2017-11-17 12:39:29 +0200

    removed unused import

--- src/test/java/org/dlearn/helsinki/skeleton/model/ClassThemeAverageTest.java
+++ src/test/java/org/dlearn/helsinki/skeleton/model/ClassThemeAverageTest.java
@@ -1,7 +1,6 @@
 package org.dlearn.helsinki.skeleton.model;
 
 import org.junit.Test;
-import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
