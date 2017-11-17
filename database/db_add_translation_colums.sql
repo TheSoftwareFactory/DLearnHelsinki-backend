416f517c4767d08efeac9590b09c988b93638e47
Kalle Lammenoja <kalle.lammenoja@gmail.com>
2017-11-17 21:42:37 +0200

    db migrate add translation colums

---
+++ database/db_add_translation_colums.sql
@@ -0,0 +1,20 @@
+ALTER TABLE "Classes"
+ADD name_fi character varying(80);
+
+ALTER TABLE "Questions"
+ADD question_fi character varying(600);
+
+ALTER TABLE "Surveys"
+ADD title_fi character varying(60);
+
+ALTER TABLE "Surveys"
+ADD description_fi character varying(100);
+
+ALTER TABLE "Themes"
+ADD title_fi character varying(50);
+
+ALTER TABLE "Themes"
+ADD title_fi character varying(50);
+
+ALTER TABLE "Themes"
+ADD description_fi character varying(256);
