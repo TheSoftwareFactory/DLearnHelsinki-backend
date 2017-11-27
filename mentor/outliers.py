632592167dda592dfe80b597a6005183ea14455c
dofajka <marko.hassinen@helsinki.fi>
2017-11-27 20:43:38 +0200

    only python core

--- mentor/outliers.py
+++ mentor/outliers.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
 
-import numpy as np
+from math import sqrt
 
-"""Computes the euclidean distance of vectors u and v.
-   1. iterates over the vector features and calculates their squared difference
-   2. sums the squared difference vector
-   3. returns the square root of the sum"""
-def euclidean_distance(u, v): return np.sqrt(np.sum([(au-av)**2 for au,av in zip(u, v)])) 
+# Computes the euclidean distance of vectors u and v 
+def euclidean_distance(u, v): return sqrt(sum([(au-av)**2 for au,av in zip(u, v)])) 
 
+def k_distance(p):
