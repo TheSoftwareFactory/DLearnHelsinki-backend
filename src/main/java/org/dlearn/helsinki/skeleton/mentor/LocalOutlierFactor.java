7120114406b9077b6580c0007806aa0d09480b1a
dofajka <marko.hassinen@helsinki.fi>
2017-12-05 13:28:05 +0200

    still missing few java implementations of the original python script

--- src/main/java/org/dlearn/helsinki/skeleton/mentor/LocalOutlierFactor.java
+++ src/main/java/org/dlearn/helsinki/skeleton/mentor/LocalOutlierFactor.java
@@ -1,33 +1,125 @@
 package org.dlearn.helsinki.skeleton.mentor;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import org.apache.commons.lang3.ArrayUtils;
 
+import org.dlearn.helsinki.skeleton.mentor.Neighbors;
+
+
+/* A Java class implementation of the paper
+ * www.dbs.ifi.lmu.de/Publikationen/Papers/LOF.pdf
+ */
 public class LocalOutlierFactor {
-	public double euclidean(double[] u, double[] v) {
-		double result = 0.0;
-		
-		if(u.length != v.length) {
-			return result;
-		}
-		
-		for(int i = 0; i < u.length; i++) {
-			result += Math.pow((u[i] - v[i]), 2);
-		}
-		
-		return result;
-	}
 
-	public double[][] kNearestNeighbors(int k, double[] p, double[][] data) {
-		double k_dist = 0.0;
-		double[][] neighbors = new double[data.length][data[0].length];
-		return neighbors;
-	}
+	/* Returns the euclidean distance of double arrays u and v
+	 * 
+	 * @param double[] u
+	 * @param double[] v
+	 * @returns double result, euclidean distance between u and v
+	 */
+    public double euclidean(double[] u, double[] v) {
+        double result = 0.0;
 
-	public static void main(String[] args) {
-		double[] u = {1,2,3};
-		double[] v = {2,2,3};
-		LocalOutlierFactor lof = new LocalOutlierFactor();
-		System.out.println(lof.euclidean(u, v));
-	}
+        if (u.length != v.length) {
+            return result;
+        }
+
+        for (int i = 0; i < u.length; i++) {
+            result += Math.pow((u[i] - v[i]), 2);
+        }
+
+        return result;
+    }
+
+    // Sort by column method missing
+    public double[][] kNearestNeighbors(int k, double[] p, double[][] data) {
+        // last index reserved for distances
+        double[][] neighbors = new double[data.length][data[0].length + 1];
+        ArrayUtils.removeElement(neighbors, p);
+        int h = 0;
+        for (int i = 0; i < neighbors.length; i++) {
+            if (Arrays.equals(p, data[i])) {
+                continue;
+            }
+            for (int j = 0; j < data[i].length; j++) {
+                neighbors[h][j] = data[i][j];
+            }
+            neighbors[h][data[i].length] = euclidean(p, data[i]);
+            h++;
+        }
+        //sort(neighbors, by=last element of every row);
+        //k_dist is the last element of the last row
+        return LocalOutlierFactor.slice(neighbors, 0, k);
+    }
+
+    /* According to the paper rechability distance of p and o is the maximum between
+     * distance(p, o) and k_dist given by the kNearestNeighbors
+     */
+    public double rechabilityDistance(int k, double[] p, double[] o,
+            double[][] data) {
+        double[][] neighbors = this.kNearestNeighbors(k, o, data); // does not care if p present
+        int n = neighbors.length - 1;
+        int m = neighbors[n].length - 1;
+        double kDist = neighbors[n][m];
+        double distance = this.euclidean(p, o);
+        return Math.max(kDist, distance);
+    }
+
+    /* Another mathematical expression in the paper
+     *
+     */
+    public double localReachabilityDensity(int k, double[] p, double[][] data) {
+    	double lrd = 0.0;
+    	double sum = 0.0;
+    	double[] neighbors = this.kNearestNeighbors(k, p, data);
+    	double[] reachDistances = new double[neighbors.length];
+    	for(int i = 0; i < reachDistances.length; i++) {
+    		reachDistances[i] = this.reschabilityDistance(k, p, neighbors[i], data);
+    	}
+    	for (double e : reachDistances) sum += e;
+    	lrd = neighbors.length / sum;
+        return lrd;
+    }
+
+    // TODO
+    public double localOutlierFactor() {
+        double lof = 0.0;
+        return lof;
+    }
+
+    // TODO
+    public void outliers(int minPts, double[][] data) {
+    }
+
+    /* Didn't find a method for double[][] slicing.
+     * Slices a double[][] by rows
+     */
+    public static double[][] slice(double[][] data, int start, int end) {
+        
+        if (end <= start) {
+            return null;
+        }
+
+        if (start > 0) {
+        	return null;
+        }
+
+        if (end > data.length) {
+        	end = data.length;
+        }
+
+        int k = 0;
+        int length = end - start;
+        double[][] result = new double[length][data[0].length];
+        
+        for (int i = start; i < end; i++) {
+            result[k] = data[i];
+            k++;
+        }
+        
+        return result;
+    }
 }
---
+++ src/test/java/org/dlearn/helsinki/skeleton/mentor/LocalOutlierFactorTest.java
@@ -0,0 +1,27 @@
+package org.dlearn.helsinki.skeleton.mentor;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+public class LocalOutlierFactorTest {
+    double[] u = { 1, 2, 3 };
+    double[] v = { 2, 2, 3 };
+    double[] w = { 3, 4, 5 };
+    double[][] data = { u, v, w };
+    LocalOutlierFactor lof = new LocalOutlierFactor();
+
+    @Test
+    public void distance() {
+
+        double difference = 1.0;
+        assertEquals(difference, lof.euclidean(u, v), 0.0);
+    }
+
+    @Test
+    public void sliceTest() {
+        double expected[][] = { v };
+        assertArrayEquals(expected, LocalOutlierFactor.slice(data, 1, 2));
+    }
+}
