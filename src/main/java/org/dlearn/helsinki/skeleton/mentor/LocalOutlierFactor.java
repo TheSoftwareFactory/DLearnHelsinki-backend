95ac7c18c16e53fa750f804a55549e6552552e40
dofajka <marko.hassinen@helsinki.fi>
2017-12-12 17:02:33 +0200

    one more case

--- src/main/java/org/dlearn/helsinki/skeleton/mentor/LocalOutlierFactor.java
+++ src/main/java/org/dlearn/helsinki/skeleton/mentor/LocalOutlierFactor.java
@@ -94,8 +94,16 @@ public class LocalOutlierFactor {
     public Map<Integer, Double> outliers(int minPts, List<Answer> rawData) {
         List<List<Answer>> data = this.prepareData(rawData);
         Map<Integer, Double> outliers = new HashMap();
+        // return empty outliers, if there is no data
         if (data.isEmpty())
             return outliers;
+        // return empty outliers if data has List:s, but
+        // all the List:s have size 0
+        boolean allMissing = true;
+        for(List<Answer> studentAnswers : data) {
+            if(studentAnswers.size() != 0) allMissing = false;
+        }
+        if(allMissing) return outliers;
         for (List<Answer> p : data) {
             double lof = this.localOutlierFactor(minPts, p, data);
             if (lof > 1.0) {
