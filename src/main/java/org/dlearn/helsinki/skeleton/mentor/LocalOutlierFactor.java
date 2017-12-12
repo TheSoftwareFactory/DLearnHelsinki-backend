fa99c66dab85a25d2bd36d48d5c1f50bb5aee28f
dofajka <marko.hassinen@helsinki.fi>
2017-12-12 13:26:51 +0200

    smarter sort for prepareData

--- src/main/java/org/dlearn/helsinki/skeleton/mentor/LocalOutlierFactor.java
+++ src/main/java/org/dlearn/helsinki/skeleton/mentor/LocalOutlierFactor.java
@@ -8,8 +8,9 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang3.ArrayUtils;
 
-import org.dlearn.helsinki.skeleton.model.Answer;
+import org.dlearn.helsinki.skeleton.mentor.AnswerComparator;
 import org.dlearn.helsinki.skeleton.mentor.Tuple;
+import org.dlearn.helsinki.skeleton.model.Answer;
 import static org.dlearn.helsinki.skeleton.mentor.Distance.euclidean;
 import static org.dlearn.helsinki.skeleton.mentor.Sort.sortMapByValue;
 import static org.dlearn.helsinki.skeleton.mentor.Sort.sortMapByValueReverse;
@@ -99,7 +100,6 @@ public class LocalOutlierFactor {
         return outliers;
     }
 
-    // Make this smarter
     // Remove List<Answer> from List<List<Answer>> data, if Answer.getAnswer() is null;
     public List<List<Answer>> prepareData(List<Answer> rawData) {
         List<Answer> tmp = new ArrayList(rawData);
@@ -107,6 +107,7 @@ public class LocalOutlierFactor {
         List<Integer> students = new ArrayList();
         List<Integer> surveys = new ArrayList();
         List<Integer> questions = new ArrayList();
+        // find out all the student_id:s, survey_id:s and question_ids:
         for (Answer ans : rawData) {
             if (!students.contains(ans.getStudent_id()))
                 students.add(ans.getStudent_id());
@@ -115,24 +116,21 @@ public class LocalOutlierFactor {
             if (!questions.contains(ans.getQuestion_id()))
                 questions.add(ans.getQuestion_id());
         }
-        Collections.sort(students);
-        Collections.sort(surveys);
-        Collections.sort(questions);
+        // Loop through every possible student_id
         for (Integer student : students) {
+            // Store all answers with same student_id into same List
             List<Answer> studentAnswers = new ArrayList();
-            for (Integer survey : surveys) {
-                for (Integer question : questions) {
-                    for (Answer ans : tmp) {
-                        if (student == ans.getStudent_id()
-                                && survey == ans.getSurvey_id()
-                                && question == ans.getQuestion_id()) {
-                            studentAnswers.add(ans);
-                            tmp.remove(ans);
-                            break;
-                        }
-                    }
+            // Loop through List<Answer> tmp
+            for(Answer ans : tmp) {
+                /* If student answer is found, add it into
+                   studentAnswers and remove it from tmp list
+                */
+                if(student == ans.getStudent_id()) {
+                    studentAnswers.add(ans);
+                    tmp.remove(ans);
                 }
             }
+            Collections.sort(studentAnswers, new AnswerComparator());
             data.add(studentAnswers);
         }
         return data;
