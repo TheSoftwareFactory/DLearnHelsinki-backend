ad734f54c35561bc775c43616b68960bd0bebe54
Eero Sääkskvuori <esaaksvu@cs.helsinki.fi>
2017-12-13 20:19:51 +0200

    fine polishing lof

--- src/main/java/org/dlearn/helsinki/skeleton/service/OutlierService.java
+++ src/main/java/org/dlearn/helsinki/skeleton/service/OutlierService.java
@@ -18,20 +18,24 @@ public class OutlierService {
        Current version has fixed minPts argument
     */
     public List<StudentLof> getClassOutliers(int class_id) {
+
+        List<StudentLof> outlierResponse = new ArrayList();
         // STATIC MINPTS FOR NOW
-        int minPts = 1;
-        LocalOutlierFactor lof = new LocalOutlierFactor();
+        int minPts = 1;      
         List<Answer> answers = db.getClassAnswers(class_id);
+        LocalOutlierFactor lof = new LocalOutlierFactor();
         Map<Integer, Double> outliers = lof.outliers(minPts, answers);
+        
         // If there are no answers in any of the class surveys
         if (outliers.isEmpty())
-            return null;
-        List<StudentLof> outlierResponse = new ArrayList();
+            return outlierResponse;
+
         for (Map.Entry<Integer, Double> entry : outliers.entrySet()) {
             int student_id = entry.getKey();
             Student student = db.getStudent(student_id);
             StudentLof stdlof = new StudentLof(student);
             double lof_score = entry.getValue();
+            stdlof.setClass_id(class_id);
             stdlof.setLofScore(lof_score);
             outlierResponse.add(stdlof);
         }
